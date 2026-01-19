5aab3ed09b34c23b9d0215280a72709101e1d7b1
Alegruz <alegruz@khu.ac.kr>
2026-01-19 22:07:03 +0900

    Enhance Windows editor support with window creation and message handling

--- include/Ludus/Editor/Pch.hpp
+++ include/Ludus/Editor/Pch.hpp
@@ -5,5 +5,10 @@
 #if defined(LUDUS_WINDOWS)
     // Windows
     #define WIN32_LEAN_AND_MEAN
+    #define NOMINMAX
+    #define UNICODE
     #include <windows.h>
+
+    constexpr wchar_t EDITOR_WINDOW_CLASS_NAME[] = L"LudusEditorWindow";
+    constexpr wchar_t EDITOR_WINDOW_TITLE[]      = L"Ludus Editor";
 #endif  // defined(LUDUS_WINDOWS)
--- src/Editor/Platform/Windows/Main.cpp
+++ src/Editor/Platform/Windows/Main.cpp
@@ -1,6 +1,48 @@
 #include <Ludus/Editor/Pch.hpp>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR commandLine, [[maybe_unused]] int commandShowFlag)
 {
+    WNDCLASSEX windowClassEx
+    {
+        .lpfnWndProc   = WindowProc,
+        .hInstance     = instance,
+        .lpszClassName = EDITOR_WINDOW_CLASS_NAME,
+    };
+
+    RegisterClassEx(&windowClassEx);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        EDITOR_WINDOW_CLASS_NAME,
+        EDITOR_WINDOW_TITLE,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        instance,
+        nullptr
+    );
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, commandShowFlag);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
