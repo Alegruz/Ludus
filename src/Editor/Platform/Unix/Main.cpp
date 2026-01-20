859345f13fb7cf84ad5a1ea93381ca7b0bfac071
Alegruz <alegruz@khu.ac.kr>
2026-01-20 17:25:20 +0900

    Add CMake support for Clang x64 builds and implement Unix main entry point

---
+++ src/Editor/Platform/Unix/Main.cpp
@@ -0,0 +1,21 @@
+#include <Ludus/Editor/Pch.hpp>
+
+#include <Ludus/Engine/Core/CommandLineManager.hpp>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	ludus::core::CommandLineManager<char> commandLineManager = ludus::core::CommandLineManager<char>::Create(argc, argv);
+	const ludus::core::DynamicArray<ludus::core::String>& arguments = commandLineManager.GetArguments();
+	for (const ludus::core::String& arg : arguments)
+	{
+		// For demonstration purposes, output each argument to the debug console
+		ludus::core::String debugOutput = "Argument: ";
+		debugOutput.Append(arg.GetData(), arg.GetSize());
+		debugOutput.PushBack('\n');
+		std::cout << debugOutput.GetData();
+	}
+
+	return 0;
+}
